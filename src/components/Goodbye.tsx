ec77ef07ab07830273e85101b2aca9867768beeb
Ka Kuen Kuen <126162398+nkk1026@users.noreply.github.com>
2023-04-12 19:42:15 +0000

    start Ant Design

--- src/components/Goodbye.tsx
+++ src/components/Goodbye.tsx
@@ -1,13 +1,11 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 const Goodbye = (props) => {
 
- const [txtColor, setTxtColor] = useState("blue");
- return (
- <>
- <h1>Goodbye {props.name}</h1>
- <h2>Current colour is: {txtColor}</h2>
- </>
-);
+  const [txtColor, setTxtColor] = useState("blue");
+  const changeColor = () => { setTxtColor("red"); }
+  const revertColor = () => { setTxtColor("blue"); }
+  return (<> <h1 onMouseEnter={changeColor} onMouseLeave={revertColor}
+    style={{ color: txtColor }}>Goodbye {props.name}</h1></>);
 }
 export default Goodbye;
--- src/components/hello.tsx
+++ src/components/hello.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
+
 const Hello = (props) => {
- //we expect props to have a property called name
- const greeting = `Hello ${props.name}`;
- return <h1>{greeting}</h1>;
+  //we expect props to have a property called name
+  const greeting = `Hello ${props.name}`;
+  return <h1>{greeting}</h1>;
 }
 export default Hello;
+
